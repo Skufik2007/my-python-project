5e948e47b0a76436271e266b3a6db5a3f32df36f
Skufik2007 <n11rw5fs@gmail.com>
2025-10-03 00:17:07 +0300

    первое задание вторйо лабараторнйо работы

---
+++ Lab 2/a.py
@@ -0,0 +1,6 @@
+import math
+print("Введите первое число")
+a = int(input())
+print("Введите второе число")
+b = int(input())
+print("Наибольший общий делитель чисел", a, "и", b, "равен", math.gcd(a, b)) 
